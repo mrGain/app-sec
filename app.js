7278a19a8fa23b67bcfff0810008461b82efc910
Rakesh Gain <rakeshgain47681@gmail.com>
2022-06-29 13:06:19 +0530

    add env var

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -21,9 +23,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisoutlittlesecret.";
+console.log(process.env.API_KEY)
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model('User' ,userSchema);
 
